68da4b7d0c77c4d1e83e59ef9d964070575d445a
taikitominari <taikitominari.co.jp@icloud.com>
2021-07-13 23:06:17 +0900

    footer-titleのfont調整

--- js/jquery.js
+++ js/jquery.js
@@ -3,7 +3,7 @@
     $(function(){
         $('a').click(function(){
             let speed = 800;
-            let href = $(this).attr("href");
+            let href = $(this).attr('href');
             let target = $(href == "#" || href == "" ? 'html' : href);
             let position = target.offset().top;
             $("html, body").animate({scrollTop:position}, speed, "swing");
